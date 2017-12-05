3d61608a2e1ed9ecadb0685ecdecf45ac5fb2e55
sTeaKOR <e1225657@student.tuwien.ac.at>
2017-12-05 01:19:14 +0100

    advent of code challenge 3 solved

--- src/advent_of_code_03.java
+++ src/advent_of_code_03.java
@@ -3,7 +3,133 @@ import java.util.Arrays;
 public class advent_of_code_03 {
 
     public static void main(String[] args) {
-        int end = 1;
+        int toBeSearched = 347991;
+        int size = 1000;
+        int[][] spiral = new int[size][size];
+        for(int[] row : spiral)
+            Arrays.fill(row,0);
+
+        int x = (size/2) - 1;
+        int y = (size/2) - 1;
+        int sum = size * size;
+        sum *= 0.8;
+        int val = 1;
+        int direction = 1;
+        spiral[y][x] = val;
+        x++;
+        val++;
+        for(int i = 0; i < sum;i++) {
+            spiral[y][x] = val;
+            if(val == toBeSearched) {
+                System.out.println("got it");
+                break;
+            }
+            val++;
+            if(direction == 1) {
+                if (spiral[y][x - 1] == 0) {
+                    direction = 2;
+                    x--;
+                } else {
+                    y--;
+                }
+            }
+            else if(direction == 2) {
+                if (spiral[y + 1][x] == 0) {
+                    direction = 3;
+                    y++;
+                } else {
+                    x--;
+                }
+            }
+            else if(direction == 3) {
+                if (spiral[y][x + 1] == 0) {
+                    direction = 4;
+                    x++;
+                } else {
+                    y++;
+                }
+            }
+            else if(direction == 4) {
+                if (spiral[y - 1][x] == 0) {
+                    direction = 1;
+                    y--;
+                } else {
+                    x++;
+                }
+            }
+        }
+
+        displayArray(spiral,size);
+
+        int steps = 0;
+        boolean found = false;
+        while(!found) {
+            if(spiral[y][x] == 1) {
+                found = true;
+                break;
+            }
+            int temp_newVal = -1;
+            int temp_x = -1;
+            int temp_y = -1;
+            if (spiral[y-1][x] != 0) {
+                if(temp_newVal == -1) {
+                    temp_newVal = spiral[y-1][x];
+                    temp_x = x;
+                    temp_y = y - 1;
+                } else {
+                    if(spiral[y-1][x] < temp_newVal) {
+                        temp_newVal = spiral[y-1][x];
+                        temp_x = x;
+                        temp_y = y - 1;
+                    }
+                }
+            }
+            if (spiral[y+1][x] != 0) {
+                if(temp_newVal == -1) {
+                    temp_newVal = spiral[y+1][x];
+                    temp_x = x;
+                    temp_y = y + 1;
+                } else {
+                    if(spiral[y+1][x] < temp_newVal) {
+                        temp_newVal = spiral[y+1][x];
+                        temp_x = x;
+                        temp_y = y + 1;
+                    }
+                }
+            }
+            if (spiral[y][x+1] != 0) {
+                if(temp_newVal == -1) {
+                    temp_newVal = spiral[y][x+1];
+                    temp_x = x + 1;
+                    temp_y = y;
+                } else {
+                    if(spiral[y][x+1] < temp_newVal) {
+                        temp_newVal = spiral[y][x+1];
+                        temp_x = x + 1;
+                        temp_y = y;
+                    }
+                }
+            }
+            if (spiral[y][x-1] != 0) {
+                if(temp_newVal == -1) {
+                    temp_newVal = spiral[y][x-1];
+                    temp_x = x - 1;
+                    temp_y = y;
+                } else {
+                    if(spiral[y][x-1] < temp_newVal) {
+                        temp_newVal = spiral[y][x-1];
+                        temp_x = x - 1;
+                        temp_y = y;
+                    }
+                }
+            }
+            System.out.println(temp_newVal);
+            x = temp_x;
+            y = temp_y;
+            steps++;
+        }
+        System.out.println(steps);
+        /*int end = 1;
         int test = 12;
         int des = 347991;
         int range = 8;
@@ -37,6 +163,22 @@ public class advent_of_code_03 {
                 }
             }
         }
-        System.out.println(step);
+        System.out.println(step);*/
+    }
+
+    private static void displayArray(int[][] spiral,int size) {
+        for(int i = 0; i < size; i++) {
+            boolean printed = false;
+            for(int j = 0; j < size; j++) {
+                if(spiral[i][j] != 0) {
+                    System.out.print(spiral[i][j]);
+                    printed = true;
+                }
+            }
+            if(printed) {
+                System.out.print("\n");
+            }
+        }
+        System.out.println("---------------------");
     }
 }
