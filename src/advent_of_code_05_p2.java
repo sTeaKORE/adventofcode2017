abfe31c66375b57d979b76523d472c00f7012794
sTeaKOR <e1225657@student.tuwien.ac.at>
2017-12-12 20:25:25 +0100

    solved day 5 part 2

---
+++ src/advent_of_code_05_p2.java
@@ -0,0 +1,50 @@
+import javax.swing.text.html.ListView;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class advent_of_code_05_p2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        List<Integer> numbers = new ArrayList<>();
+        while(in.hasNextInt()) {
+            numbers.add(in.nextInt());
+        }
+        in.close();
+        display(numbers,0);
+        int atm = 0;
+        int steps = 0;
+        while(atm >= 0 && atm < numbers.size()) {
+            int toJump = numbers.get(atm);
+            alter(numbers,atm);
+            atm += toJump;
+            steps++;
+            //System.out.println("At: " + atm);
+            //display(numbers,atm);
+        }
+        System.out.println(steps);
+    }
+
+    private static List<Integer> alter(List<Integer> temp, int index) {
+        int tmp = temp.get(index);
+        if(tmp >= 3) {
+            tmp--;
+        } else {
+            tmp++;
+        }
+        temp.set(index, tmp);
+        return temp;
+    }
+
+    private static void display(List<Integer> numbers,int atm) {
+        for(int i = 0; i < numbers.size();i++) {
+            if(i == atm) {
+                System.out.print("(" + numbers.get(i) + ") ");
+            } else {
+                System.out.print(numbers.get(i) + " ");
+            }
+        }
+        System.out.println("\n");
+    }
+}
